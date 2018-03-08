628c87c3de68754cc9d7593e1b0061495acf2b9f
SkyJedi <skyjedi@gmail.com>
2018-03-07 19:05:14 -0600

    deepmerge in reducers

--- src/reducers/changeState.js
+++ src/reducers/changeState.js
@@ -1,5 +1,6 @@
 import * as initialState from './initialState';
 import * as data from '../data';
+import merge from 'deepmerge';
 
 export const fix = (state = true, action) =>{
     if (action.type === 'fix_Changed')  return action.payload;
@@ -224,7 +225,7 @@ export const customTalents = (state = {}, action) =>{
 export const skills = (state = data.skills, action) => {
     if (action.type === 'customSkills_Changed') {
         if (action.payload === null) return data.skills;
-        else return {...action.payload, ...data.skills};
+        else return merge(data.skills, action.payload);
     }
     return state;
 };
@@ -232,7 +233,7 @@ export const skills = (state = data.skills, action) => {
 export const archetypes = (state = data.archetypes, action) => {
     if (action.type === 'customArchetypes_Changed') {
         if (action.payload === null) return data.archetypes;
-        else return {...action.payload, ...data.archetypes};
+        else return merge(data.archetypes, action.payload);
     }
     return state;
 };
@@ -240,7 +241,7 @@ export const archetypes = (state = data.archetypes, action) => {
 export const careers = (state = data.careers, action) => {
     if (action.type === 'customCareers_Changed') {
         if (action.payload === null) return data.careers;
-        else return {...action.payload, ...data.careers};
+        else return merge(data.careers, action.payload);
     }
     return state;
 };
@@ -248,7 +249,7 @@ export const careers = (state = data.careers, action) => {
 export const motivations = (state = data.motivations, action) => {
     if (action.type === 'customMotivations_Changed') {
         if (action.payload === null) return data.motivations;
-        else return {...action.payload, ...data.motivations};
+        else return merge(data.motivations, action.payload);
     }
     return state;
 };
@@ -256,7 +257,7 @@ export const motivations = (state = data.motivations, action) => {
 export const talents = (state = data.talents, action) => {
     if (action.type === 'customTalents_Changed') {
         if (action.payload === null) return data.talents;
-        else return {...action.payload, ...data.talents};
+        else return merge(data.talents, action.payload);
     }
     return state;
 };
@@ -264,7 +265,7 @@ export const talents = (state = data.talents, action) => {
 export const qualities = (state = data.qualities, action) => {
     if (action.type === 'customQualities_Changed') {
         if (action.payload === null) return data.qualities;
-        else return {...action.payload, ...data.qualities};
+        else return merge(data.qualities, action.payload);
     }
     return state;
 };
