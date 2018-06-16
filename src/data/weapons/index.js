10d29b3fe0959d97ca578505f20dc1673a8c52e2
Nick Holmstedt <holmstn@gmail.com>
2018-06-16 11:52:43 -0400

    new weapon data

---
+++ src/data/weapons/index.js
@@ -0,0 +1,36 @@
+import {default as CRB} from './CRB'
+import {default as ROT} from './ROT'
+import merge from 'deepmerge';
+//import { intersection } from 'lodash-es'
+
+// Unneeded... as long as weapons don't change b/w versions
+/*
+const imports = [CRB, ROT]
+
+const findDupes = (typeObjects) => {
+    let keys = typeObjects.map(obj => {
+        return Object.keys(obj)
+    })
+
+    return intersection(...keys)
+}
+
+let dupes = findDupes(imports)
+
+let mergedImports = imports.map(weaponData => {
+    return Object.keys(weaponData).reduce((acc,weapon) => {
+        if (dupes.includes(weapon)) {
+            let newKey = `${weapon} (${weapon["book"]})`
+            return Object.assign(acc, {
+                [newKey]: weapon
+            }) 
+        }
+
+        return weapon
+    }, {})
+})
+
+export const weapons = Object.assign({}, ...mergedImports)
+*/
+
+export const weapons = merge.all([CRB, ROT]);
